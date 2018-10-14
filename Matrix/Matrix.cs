8cd8f29810620579466ca87194656a982eb65735
Богомолов Илья <schredinger@mail.ru>
2018-10-14 11:55:04 +0700

    Добавлены методы GetRow() и SetRow()

--- Matrix/Matrix.cs
+++ Matrix/Matrix.cs
@@ -3,26 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RowVector= Vector.Vector;
 
 namespace Matrix
 {
     class Matrix
     {
-        private Vector.Vector[] _data;
+        private RowVector[] _data;
 
         public Matrix(int n, int m)
         {
-            _data = new Vector.Vector[m];
+            _data = new RowVector[m];
             for (int i = 0; i < m; i++)
             {
-                _data[i] = new Vector.Vector(n);
+                _data[i] = new RowVector(n);
             }
+        }
+
+        public string GetSize()
+        {
+            return $"{_data[0].GetSize()}x{_data.Length}";
+        }
+
+        public RowVector GetRow(int index)
+        {
+            return _data[index];
+        }
+
+        public void SetRow(RowVector vector, int index)
+        {
+            var resultVector = new RowVector(_data[0].GetSize());
+            Array.Copy(vector.Components, resultVector.Components, Math.Min(vector.GetSize(), resultVector.GetSize()));
+            _data[index] = resultVector;
         }
 
         public override string ToString()
         {
-
-            return " ";
+            string result = string.Empty;
+            foreach (var vector in _data)
+            {
+                result += vector;
+            }
+            return $"{{{result}}}";
         }
     }
 }
--- Matrix/MatrixProgram.cs
+++ Matrix/MatrixProgram.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RowVector = Vector.Vector;
 
 namespace Matrix
 {
@@ -13,6 +14,13 @@ namespace Matrix
             var matrix = new Matrix(3,4);
 
             Console.WriteLine(matrix);
+            Console.WriteLine(matrix.GetSize());
+
+            var vector = new RowVector(new double[] {1, 2});
+
+            matrix.SetRow(vector,1);
+            Console.WriteLine(matrix);
+
             Console.ReadLine();
 
         }
