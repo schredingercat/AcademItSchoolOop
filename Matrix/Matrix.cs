3358dda0eedf07880b2ccd2b77e57dac5110bb76
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2018-10-15 15:49:49 +0700

    Добавлены конструкторы, методы GetColumn(), SetColumn(), транспонирование, умножение на скаляр

--- Matrix/Matrix.cs
+++ Matrix/Matrix.cs
@@ -20,6 +20,41 @@ namespace Matrix
             }
         }
 
+        public Matrix(Matrix matrix)
+        {
+            var size = matrix._data.Length;
+            _data = new RowVector[size];
+            for (int i = 0; i < size; i++)
+            {
+                _data[i] = new RowVector(matrix._data[i]);
+            }
+        }
+
+        public Matrix(double[,] dataValues)
+        {
+            _data = new RowVector[dataValues.GetLength(0)];
+
+            for (int i = 0; i < dataValues.GetLength(0); i++)
+            {
+                _data[i] = new RowVector(dataValues.GetLength(1));
+                for (int j = 0; j < dataValues.GetLength(1); j++)
+                {
+                    _data[i].Components[j] = dataValues[i, j];
+                }
+            }
+        }
+
+        public Matrix(RowVector[] data)
+        {
+            var size = data.Length;
+            _data = new RowVector[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                _data[i] = new RowVector(data[i]);
+            }
+        }
+
         public string GetSize()
         {
             return $"{_data[0].GetSize()}x{_data.Length}";
@@ -35,6 +70,44 @@ namespace Matrix
             var resultVector = new RowVector(_data[0].GetSize());
             Array.Copy(vector.Components, resultVector.Components, Math.Min(vector.GetSize(), resultVector.GetSize()));
             _data[index] = resultVector;
+        }
+
+        public RowVector GetColumn(int index)
+        {
+            var resultVector = new RowVector(_data.Length);
+            for (int i = 0; i < _data.Length; i++)
+            {
+                resultVector.Components[i] = _data[i].Components[index];
+            }
+
+            return resultVector;
+        }
+
+        public void SetColumn(RowVector vector, int index)
+        {
+            for (int i = 0; i < _data.Length; i++)
+            {
+                _data[i].Components[index] = vector.Components[i];
+            }
+        }
+
+        public void Transpone()
+        {
+            var resultData = new RowVector[_data[0].GetSize()];
+            for (int i = 0; i < resultData.Length; i++)
+            {
+                resultData[i] = new RowVector(GetColumn(i));
+            }
+
+            _data = resultData;
+        }
+
+        public void Multiply(double factor)
+        {
+            foreach (var vector in _data)
+            {
+                vector.Multiply(factor);
+            }
         }
 
         public override string ToString()
--- Matrix/MatrixProgram.cs
+++ Matrix/MatrixProgram.cs
@@ -11,15 +11,38 @@ namespace Matrix
     {
         static void Main()
         {
-            var matrix = new Matrix(3,4);
+            var matrix1 = new Matrix(3,4);
+            Console.WriteLine(matrix1);
+            Console.WriteLine(matrix1.GetSize());
+            
+            var matrix2 = new Matrix(matrix1);
+            Console.WriteLine(matrix2);
+            Console.WriteLine(matrix2.GetSize());
 
-            Console.WriteLine(matrix);
-            Console.WriteLine(matrix.GetSize());
+            var matrix3 = new Matrix(new double[,]{{1,2,3},{4,5,6}});
+            Console.WriteLine(matrix3);
+            Console.WriteLine(matrix3.GetSize());
 
-            var vector = new RowVector(new double[] {1, 2});
+            var matrix4 = new Matrix(new RowVector[]{new RowVector(new double[]{12,23,34,56}), new RowVector(new double[] { 65, 83, 45, 44 }), new RowVector(new double[] { 3, 5, 8, 9 }) });
+            Console.WriteLine(matrix4);
+            Console.WriteLine(matrix4.GetSize());
 
-            matrix.SetRow(vector,1);
-            Console.WriteLine(matrix);
+
+            Console.WriteLine($"Строка {matrix4.GetRow(1)}");
+            matrix4.SetRow(new RowVector(new double[] { 18, 19, 20, 21 }), 0);
+            Console.WriteLine(matrix4);
+
+            Console.WriteLine($"Столбец {matrix4.GetColumn(1)}");
+            matrix4.SetColumn(new RowVector(new double[] { 16, 17, 25 }), 2);
+            Console.WriteLine(matrix4);
+
+            Console.WriteLine();
+            matrix4.Transpone();
+            Console.WriteLine(matrix4);
+            Console.WriteLine(matrix4.GetSize());
+
+            matrix4.Multiply(10);
+            Console.WriteLine(matrix4);
 
             Console.ReadLine();
 
