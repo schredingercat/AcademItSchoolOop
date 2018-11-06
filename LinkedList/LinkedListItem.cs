11ceeaedb1004471e90edc108c0e4cfbc1115a63
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2018-11-06 13:11:44 +0700

    Добавлен проект LinkedList

---
+++ LinkedList/LinkedListItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedListItem<T>
+    {
+        public T Data { get; set; }
+        public LinkedListItem<T> Next { get; set; }
+
+        public LinkedListItem(T data)
+        {
+            Data = data;
+        }
+
+        public LinkedListItem(T data, LinkedListItem<T> next)
+        {
+            Data = data;
+            Next = next;
+        }
+    }
+}
