10f9ea3fa7a675d0ac1521e6cd79aba6b6f1c22e
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2018-11-27 14:45:03 +0700

    Добавлен проект Разбор CSV

---
+++ CsvParse/CsvToHtml.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+
+namespace CsvParse
+{
+    public static class CsvToHtml
+    {
+        public static string ConvertCsvToHtml(string input)
+        {
+            const string openTags = "<!doctype html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n<title>Converted from CSV</title>\n</head>\n<body>\n<table border=\"1\">\n<tr>\n<td>";
+            const string closeTags = "</td>\n</tr>\n</table>\n</body>\n</html>";
+
+            input = input.Replace("&", "&amp");
+            input = input.Replace("<", "&lt");
+            input = input.Replace(">", "&gt");
+
+            input = input.Replace("\r\n", "\n");
+            input = input.Replace("\r", "\n");
+
+            var stringLength = input.Length;
+
+            var firstInCell = true;
+            var skipNext = false;
+            var openEscape = false;
+
+            var output = new StringBuilder();
+
+            output.Append(openTags);
+
+            for (var i = 0; i < stringLength; i++)
+            {
+                if (skipNext)
+                {
+                    skipNext = false;
+                    continue;
+                }
+
+                var _char = input[i];
+
+                switch (_char)
+                {
+                    case '"':
+                        if (i < stringLength - 1
+                            && input[i + 1] == '"'
+                            && !firstInCell)
+                        {
+                            output.Append('"');
+                            skipNext = true;
+                        }
+                        else
+                        {
+                            openEscape = !openEscape;
+                        }
+                        firstInCell = false;
+                        break;
+                    case ',':
+                        if (openEscape)
+                        {
+                            output.Append(',');
+                        }
+                        else
+                        {
+                            output.Append("</td><td>");
+                            firstInCell = true;
+                        }
+                        break;
+                    case '\n':
+                        if (openEscape)
+                        {
+                            output.Append("<br/>");
+                        }
+                        else
+                        {
+                            output.Append("</td>\n</tr>\n<tr>\n<td>");
+                            firstInCell = true;
+                        }
+                        break;
+                    default:
+                        output.Append(_char);
+                        firstInCell = false;
+                        break;
+                }
+            }
+
+            output.Append(closeTags);
+
+            return output.ToString();
+        }
+    }
+}
---
+++ CsvParse/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace CsvParse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSelectFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog { Filter = "CSV файлы (*.csv)|*.csv|Все файлы (*.*)|*.*" };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                TextBoxFileName.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void ButtonSaveHtml_OnClick(object sender, RoutedEventArgs e)
+        {
+            string inputString;
+
+            try
+            {
+                inputString = File.ReadAllText(TextBoxFileName.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка открытия файла\n{ex.Message}");
+                return;
+            }
+
+            var outputString = CsvToHtml.ConvertCsvToHtml(inputString);
+
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "HTML файлы (*.html)|*.html|Все файлы (*.*)|*.*",
+                    FileName = "output.html"
+                };
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    File.WriteAllText(saveFileDialog.FileName, outputString);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка сохранения файла\n{ex.Message}");
+            }
+        }
+
+    }
+}
