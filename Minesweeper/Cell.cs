57aec8a5e0816bd9d7dc2670c6727e2e5941eec4
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2018-12-07 13:42:42 +0700

    Создан проект Minesweeper

---
+++ Minesweeper/Cell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    public class Cell
+    {
+        public bool Mine { get; set; }
+        public bool Open { get; set; }
+        public bool Marked { get; set; }
+    }
+}
---
+++ Minesweeper/Field.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Minesweeper
+{
+    public class Field
+    {
+        private Cell[,] _cells;
+
+
+        Field(int columns, int rows)
+        {
+            _cells = new Cell[columns, rows];
+        }
+
+        public void PlaceMines(int minesCount, int firstX, int firstY)
+        {
+            var columns = _cells.GetLength(0);
+            var rows = _cells.GetLength(1);
+            var random = new Random();
+
+            while (minesCount > 0)
+            {
+                var x = random.Next(columns);
+                var y = random.Next(rows);
+
+                if (!_cells[x, y].Mine && x != firstX && y != firstY)
+                {
+                    _cells[x, y].Mine = true;
+                    minesCount--;
+                }
+            }
+        }
+
+    }
+}
